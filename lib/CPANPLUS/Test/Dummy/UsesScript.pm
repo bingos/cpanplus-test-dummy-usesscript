8407f72d11ea5c40d3d83aec5858d677335c4e6e
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2012-03-30 22:53:48 +0100

    Mojo rising

---
+++ lib/CPANPLUS/Test/Dummy/UsesScript.pm
@@ -0,0 +1,25 @@
+package CPANPLUS::Test::Dummy::UsesScript;
+
+#ABSTRACT: CPANPLUS Test Dummy
+
+use strict;
+use warnings;
+
+q[Test me oh test me, test me if you think you've heard this one before];
+
+=pod
+
+=head1 SYNOPSIS
+
+ # erm
+
+ cpanp -i CPANPLUS::Test::Dummy::UsesScript
+
+=head1 DESCRIPTION
+
+CPANPLUS::Test::Dummy::UsesScript is a fake module that has a script file
+so I can test something in L<CPANPLUS> and L<CPANPLUS::YACSmoke>
+
+No moving parts and nothing to see.
+
+=cut
